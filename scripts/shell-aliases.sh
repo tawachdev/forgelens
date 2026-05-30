84e87376549c351d10d3b706dcea53df7876a490
tawachdev <tawachmohmmed399@gmail.com>
2026-05-30 23:23:45 +0100

    chore: add optional shell aliases for quick commands

---
+++ scripts/shell-aliases.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Source this file from your shell config:
+# source /Users/mohmmedtawach/forgelens/scripts/shell-aliases.sh
+
+alias fl='pnpm dev'
+alias fls='pnpm scan'
+alias flu='pnpm ui'
+alias flh='pnpm health'
+alias flc='pnpm clean'
+alias flsave='pnpm save'
+alias fldiff='pnpm diff'
+alias flctx='pnpm context'
